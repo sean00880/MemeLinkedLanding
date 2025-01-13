414292e26cce56623695bd03b4741abe43e6a92f
sean00880 <50418518+sean00880@users.noreply.github.com>
2025-01-13 16:49:04 -0500

    new 5

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -146,7 +146,7 @@ const CarouselSection: React.FC = () => {
 
         {/* Quadrants Layout for lg and Above */}
         <div className="relative flex flex-col gap-4 lg:block ">
-          <div className="relative lg:absolute lg:top-[15vh] xl:top-[10vh] lg:left-[5%] w-[100%] lg:w-[35%] xl:w-[30%]">
+          <div className="relative lg:absolute lg:top-[13vh] xl:top-[10vh] lg:left-[5%] w-[100%] lg:w-[35%] xl:w-[30%]">
             <div className="neumorphic-card">
               <h4 className="text-lg font-bold text-green-400">
                 {slides[currentIndex].features[0].title}
@@ -156,7 +156,7 @@ const CarouselSection: React.FC = () => {
               </p>
             </div>
           </div>
-          <div className="relative lg:absolute lg:top-[15vh] xl:top-[10vh] lg:right-[5%] w-[100%] lg:w-[35%] xl:w-[30%]">
+          <div className="relative lg:absolute lg:top-[13vh] xl:top-[10vh] lg:right-[5%] w-[100%] lg:w-[35%] xl:w-[30%]">
             <div className="neumorphic-card">
               <h4 className="text-lg font-bold text-green-400">
                 {slides[currentIndex].features[1].title}
