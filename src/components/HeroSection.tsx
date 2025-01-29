e45e72b01e1d44958471853f331a0cfd7e6c5e0f
sean00880 <50418518+sean00880@users.noreply.github.com>
2025-01-28 19:03:48 -0500

    new10

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -104,7 +104,7 @@ const HeroSection: React.FC = () => {
                 className="socials border-2 border-purple-400 hover:scale-110 hover:drop-shadow-lg transition-transform rounded-full"
               />
             </Link>
-            <Link href="https://tickertrending.com/memelinked">
+            <Link href="https://www.tickertrending.com/ticker/MK">
               <Image
                 src="/icons/TickerTrending.jpg"
                 width={50}
